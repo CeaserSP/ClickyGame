3ff72b755807a765f693ed229375791cecee6e33
Sheldon Perkins <csperkins18@gmail.com>
2020-01-03 15:34:19 -0500

    setup paper mdc

---
+++ clickygame/src/components/paper/paper.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+
+const paper = props => (
+    <Paper>
+        {props.children}
+    </Paper>
+)
+
+export default paper;
