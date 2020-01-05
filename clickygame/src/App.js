df30aa3d9ed22c582c83c8dbb32256ac13a3000f
Sheldon Perkins <csperkins18@gmail.com>
2020-01-05 13:50:03 -0500

    gameOver function made

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -12,6 +12,22 @@ class App extends Component {
     score: 0
   };
   // Calculate score and check if topscore should be updated and get allert end of game
- 
+  gameOver = () => {
+    if (this.state.score > this.state.topscore) {
+      this.setState({topscore: this.state.score}, function() {
+        console.log(this.state.highscore);
+      });
+    }
+    this.state.cards.forEach(card => {
+      card.count = 0;
+    });
+    alert(`Game Over :( \nscore: ${this.state.score}`);
+    this.setState({score: 0});
+    return true;
+  }
+
+
+  
+};
 
 export default App;
