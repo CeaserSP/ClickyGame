779c7bb0a684c50272e04df07a76b4e8958c26cc
Sheldon Perkins <csperkins18@gmail.com>
2020-01-05 13:45:21 -0500

    add top score

--- clickygame/src/components/score/score.js
+++ clickygame/src/components/score/score.js
@@ -5,7 +5,7 @@ const Score = props => (
     <div className="header">
         <div className="title">{props.children}</div>
         <div className="scores">
-            Score:{props.score}
+            Score:{props.score} Top score: {props.topscore}
         </div>
     </div>
 )
