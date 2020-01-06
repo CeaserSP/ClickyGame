dedea808d413c180e0c2182fe9df273bc0b575ab
Sheldon Perkins <csperkins18@gmail.com>
2020-01-06 17:46:32 -0500

    Updates

---
+++ precache-manifest.959b9b8c45793ec1db1f4eecd59e8260.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a17e7eb9c3f43590daafb82db4216a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41ce2fea54581de807c0",
+    "url": "/static/css/main.0ad1447c.chunk.css"
+  },
+  {
+    "revision": "177e8dcc1f24fef70121",
+    "url": "/static/js/2.e0865fc2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e0865fc2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41ce2fea54581de807c0",
+    "url": "/static/js/main.2d1eaeef.chunk.js"
+  },
+  {
+    "revision": "2dbe864d95ddaf8dd2ad",
+    "url": "/static/js/runtime-main.81a35a37.js"
+  },
+  {
+    "revision": "1af2a7a1128793d931eaae7913546e51",
+    "url": "/static/media/quinn.1af2a7a1.jpg"
+  }
+]);
